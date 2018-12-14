f32cd372f9bf3a6a66069aa61e9a3429511d9707
Glitch (glacier-road) <none>
2018-12-14 18:44:42 +0000

    😜🎁 Updated with Glitch

--- server.js
+++ server.js
@@ -8,19 +8,28 @@ const bcrypt = require('bcrypt');
 fccTesting(app); //For FCC testing purposes
 
 const saltRounds = 12;
-const myPlaintextPassword = 'sUperpassw0rd!';
+const myPlaintextPassword = 'passw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res); //true
+  });
+});
+
 
 
 
 //END_ASYNC
 
 //START_SYNC
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
 
 //END_SYNC
 
